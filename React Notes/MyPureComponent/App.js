3b353e25d468acbe9d54a2f2fbf4134e3e21ec2a
M3RCURRRY <49792824+M3RCURRRY@users.noreply.github.com>
2022-08-04 16:12:23 +0300

    Update App.js

--- React Notes/MyPureComponent/App.js
+++ React Notes/MyPureComponent/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 class MyPureComponent extends React.Component {
   shouldComponentUpdate(nextProps, nextState) {
@@ -37,22 +37,19 @@ class Test extends MyPureComponent {
     super(props);
 
     this.state = {
-      counter: 0,
-      prop1: props.dep1,
-      prop2: props.dep2
+      counter: 0
     };
   }
 
   counterHandler() {
     this.setState((prev) => ({
-      ...prev,
       counter: prev.counter + 1
     }));
   }
 
   noChangeHandler() {
+    console.log(this.props);
     this.setState((prev) => ({
-      ...prev,
       counter: prev.counter
     }));
   }
@@ -61,10 +58,12 @@ class Test extends MyPureComponent {
     console.log("Render");
     return (
       <div>
-        <p>{this.props.dep1}</p>
-        <p>{this.props.dep2.dep}</p>
+        <p>{this.props.d1}</p>
+        <p>{this.props.d2.dep}</p>
         <h2>{this.state.counter}</h2>
-        <button onClick={() => this.counterHandler()}>Increment</button>
+        <button onClick={() => this.counterHandler()}>
+          Update with changes
+        </button>
         <button onClick={() => this.noChangeHandler()}>
           Update state without changes
         </button>
@@ -74,25 +73,29 @@ class Test extends MyPureComponent {
 }
 
 export default function App() {
-  let dep1 = "Number";
-  let dep2 = {
-    dep: "Object"
-  };
+  const [isPrimChanged, setPrimChanged] = useState(false);
+  const [isObjectChanged, setObjectChanged] = useState(false);
 
-  let newDep1 = "String";
-  let newDep2 = {
-    dep: "NewObject"
-  };
+  function primChangeHandler() {
+    setPrimChanged(!isPrimChanged);
+  }
 
-  function buttonHandler() {
-    dep1 = newDep1;
-    dep2 = newDep2;
+  function objectChangeHandler() {
+    setObjectChanged(!isObjectChanged);
   }
 
   return (
     <div>
-      <Test dep1={dep1} dep2={dep2} />
-      <button onClick={buttonHandler}>Change props</button>
+      <button onClick={primChangeHandler}>Change primitive prop</button>
+      <button onClick={objectChangeHandler}>Change object prop</button>
+      <Test
+        d1={isPrimChanged ? "Changed" : "Unchanged"}
+        d2={
+          isObjectChanged
+            ? { dep: "ObjectChanged" }
+            : { dep: "ObjectUnchanged" }
+        }
+      />
     </div>
   );
 }
